f85a0a5eec4c8ed8dcedbb7b5ffca318b5f3ba4f
Rodric Rabbah <rodric@gmail.com>
2020-01-23 00:04:13 -0500

    Workaround bug in runtime.

--- packages/default/web-sms/create_twilio_message.py
+++ packages/default/web-sms/create_twilio_message.py
@@ -4,9 +4,13 @@ from twilio.rest import Client
 # Create auth and configure numbers
 account_sid = os.getenv('ACCOUNT_SID')
 auth_token = os.getenv('AUTH_TOKEN')
-client = Client(account_sid, auth_token)
+client = None
 
 def create_twilio_message(body_message, to_number, from_number):
+    global client  # workaround runtime proxy bug
+    if client is None: # part of workaround, init client once
+       client = Client(account_sid, auth_token) # as this should be in the globals
+
     message = client.messages \
                         .create(
                             body = body_message,
